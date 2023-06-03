722300cba116c6acf94c500269e3f04ecb274129
Ivan <ivan120536@gmail.com>
2023-06-03 18:15:03 +0400

    aaaaaa

--- app/routers/article.py
+++ app/routers/article.py
@@ -2,7 +2,7 @@ from fastapi import Request, Depends
 from fastapi.routing import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.schemas import Article
 
@@ -16,7 +16,11 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/articles/{id}", response_class=HTMLResponse)
 async def get_article(request: Request, id: int, db: Session = Depends(get_db)):
     db_article = get_article_by_id(db, id)
+
+    if not db_article:
+        raise HTTPException(status_code=404)
     article = Article.from_orm(db_article)
+
     return templates.TemplateResponse("article.html", {
         "request": request,
         **article.dict()
