52d64307e09fa4bd9c068db170379e82eb6f8141
KamilM1205 <bigprogrammer1205@gmail.com>
2023-06-03 22:14:37 +0400

    -

--- app/routers/article.py
+++ app/routers/article.py
@@ -1,19 +1,25 @@
-from fastapi import Request
+from fastapi import Depends, Request
 from fastapi.routing import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+
+from sqlalchemy.orm import Session
+
+from app.crud import get_article_by_id, get_articles
+from app.dependencies import get_db
 
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/articles/{id}", response_class=HTMLResponse)
-async def get_article(request: Request):
+async def get_article(request: Request, id: int, db: Session = Depends(get_db)):
+    article = get_article_by_id(db, id)
     return templates.TemplateResponse("article.html", {
             "request": request, 
-            "title": title,
-            "content": content,
-            "author": author,
-            "email": email,
-            "phone": phone,
+            "title": article.title,
+            "content": article.content,
+            "author": article.author,
+            "email": article.email,
+            "phone": article.phone,
         })
