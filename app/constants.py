c0ac95e8e3fdead8a94a10912e6b80e69ee4c18c
KamilM1205 <bigprogrammer1205@gmail.com>
2023-06-03 20:40:12 +0400

    Added article list, article viewer

---
+++ app/constants.py
@@ -0,0 +1,1 @@
+APP_DOMAIN = "localhost:8000"
--- app/main.py
+++ app/main.py
@@ -3,14 +3,20 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from app.utils.article_generator import get_list
+
+from app.routers import article
+
 
 app = FastAPI()
-
 
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+app.include_router(article.router)
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    articles = get_list()
+    return templates.TemplateResponse("index.html", {"request": request, "articles": articles})
